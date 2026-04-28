9350ba84c932eef695476a7c507cbcdd82fb5de6
shubhampawar17 <sp1180253@gmail.com>
2026-04-28 16:24:25 +0530

    UI changes & login issue resolved

--- InsuranceBackend/InsuranceProject/Data/Context.cs
+++ InsuranceBackend/InsuranceProject/Data/Context.cs
@@ -1,4 +1,5 @@
-﻿using InsuranceProject.Models;
+using InsuranceProject.Models;
+using InsuranceProject.Types;
 using Microsoft.EntityFrameworkCore;
 
 namespace InsuranceProject.Data
@@ -13,7 +14,7 @@ namespace InsuranceProject.Data
         public DbSet<Policy> Policies { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<State> States { get; set; }
+        public DbSet<InsuranceProject.Models.State> States { get; set; }
         public DbSet<City> Cities { get; set; }
 
         public DbSet<Claimm> claims { get; set; }
@@ -40,6 +41,17 @@ namespace InsuranceProject.Data
 
         public DbSet<PolicyType> PolicyTypes { get; set; }
         public Context(DbContextOptions<Context> options) : base(options) { }
-       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = new Guid("8c1bc05b-21a8-4b86-50bc-08dd115dd6c7"), RoleName = InsuranceProject.Types.Roles.ADMIN, BaseId = new Guid("11111111-1111-1111-1111-111111111111"), IsDeleted = false },
+                new Role { Id = new Guid("9d9c16e3-8eb7-4e8c-50bd-08dd115dd6c7"), RoleName = InsuranceProject.Types.Roles.EMPLOYEE, BaseId = new Guid("22222222-2222-2222-2222-222222222222"), IsDeleted = false },
+                new Role { Id = new Guid("a8f1b121-fd38-4733-50be-08dd115dd6c7"), RoleName = InsuranceProject.Types.Roles.AGENT, BaseId = new Guid("33333333-3333-3333-3333-333333333333"), IsDeleted = false },
+                new Role { Id = new Guid("74c70ef9-b3f4-4e6d-50bb-08dd115dd6c7"), RoleName = InsuranceProject.Types.Roles.CUSTOMER, BaseId = new Guid("44444444-4444-4444-4444-444444444444"), IsDeleted = false }
+            );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ InsuranceBackend/InsuranceProject/Migrations/20260428084057_SeedRolesData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace InsuranceProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRolesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "BaseId", "IsDeleted", "RoleName" },
+                values: new object[,]
+                {
+                    { new Guid("74c70ef9-b3f4-4e6d-50bb-08dd115dd6c7"), new Guid("44444444-4444-4444-4444-444444444444"), false, 3 },
+                    { new Guid("8c1bc05b-21a8-4b86-50bc-08dd115dd6c7"), new Guid("11111111-1111-1111-1111-111111111111"), false, 0 },
+                    { new Guid("9d9c16e3-8eb7-4e8c-50bd-08dd115dd6c7"), new Guid("22222222-2222-2222-2222-222222222222"), false, 1 },
+                    { new Guid("a8f1b121-fd38-4733-50be-08dd115dd6c7"), new Guid("33333333-3333-3333-3333-333333333333"), false, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("74c70ef9-b3f4-4e6d-50bb-08dd115dd6c7"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("8c1bc05b-21a8-4b86-50bc-08dd115dd6c7"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("9d9c16e3-8eb7-4e8c-50bd-08dd115dd6c7"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("a8f1b121-fd38-4733-50be-08dd115dd6c7"));
+        }
+    }
+}
